b653a466d7c1451ff1611e62d3f2088167f030a0
dncuong <dinhcuongnd2001@gmail.com>
2024-01-10 14:48:36 +0700

    feat: signup

--- api/controllers/auth/signup.js
+++ api/controllers/auth/signup.js
@@ -26,23 +26,45 @@ module.exports = {
     },
   },
 
-  exits: {},
+  exits: {
+    emailExist: {
+      description: "Your email is already registered",
+      statusCode: "409",
+    },
+
+    serverError: {
+      description: "Something went wrong",
+      statusCode: "500",
+    },
+  },
 
   fn: async function (inputs, exits) {
+    let checkEmail = false;
+    try {
+      const {
+        data: { check },
+      } = await sails.helpers.checkEmail.with({
+        email: inputs.email,
+      });
+      checkEmail = check;
+    } catch (error) {
+      console.log("error :", error);
+    }
+    if (checkEmail) throw "emailExist";
+
     const userRecord = await User.create({
       firstName: inputs.firstName,
       lastName: inputs.lastName,
       email: inputs.email,
       password: inputs.password,
     }).fetch();
-    if (!userRecord) {
-      return exits.invalid({
-        message: "Sign up fail",
-      });
-    }
+
+    if (!userRecord) throw "serverError";
+
     return exits.success({
       message: "User has been created successfully.",
       data: userRecord,
+      statusCode: 201,
     });
   },
 };
---
+++ api/helpers/check-email.js
@@ -0,0 +1,34 @@
+module.exports = {
+  friendlyName: "Check Exist Email",
+
+  description: "",
+
+  inputs: {
+    email: {
+      type: "string",
+      example: "example@gmail.com",
+      description: "The name of the person to greet.",
+      require: true,
+    },
+  },
+
+  exits: {},
+
+  fn: async function ({ email }, exits) {
+    const check = await User.find({
+      email,
+    });
+
+    if (check.length)
+      return exits.success({
+        message: "User has been created successfully.",
+        data: { check: true },
+        statusCode: 201,
+      });
+    return exits.invalid({
+      message: "Email has been existent",
+      data: { check: false },
+      statusCode: 409,
+    });
+  },
+};
